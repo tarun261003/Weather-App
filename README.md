2da05b474ee54678d74b3e651ece779a5ff03eac
tarun261003 <tarunmangalampalli@gmail.com>
2024-05-02 22:54:13 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+# ☀️ Explore the Weather: React-based Weather App
+
+🌦️ Delighted to present my latest creation: a sleek and intuitive Weather App developed using React.js! 💻✨
+
+With this app, you can effortlessly stay informed about current weather conditions, forecasts, and more. Featuring a user-friendly interface and real-time updates, it's your go-to companion for planning outdoor activities or staying ahead of changing weather patterns.
+
+![Weather App Screenshot](https://github.com/krishnateja-81/Weather-App/blob/master/weather.JPG)
+
+## Key Features:
+- 🔍 Location-based weather data retrieval
+- 🌡️ Current weather conditions including temperature, humidity, and wind speed
+- 🌅 5-day weather forecast for comprehensive planning
+- 📅 Ability to view weather trends over time
+- 📱 Responsive design for seamless access across devices
+
+## Technologies
+- React.js
+- Weather API (mention the specific API used)
+
+## Website
+Check out the Weather app [here](https://kt-react-weatherapp.netlify.app/)
+=======
+# Weather-App
+>>>>>>> 8d3f6171626271d46bcc6e66437c7c6f24475375
---
+++ src/App.js
@@ -0,0 +1,127 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Temp from "./components/temp";
+import Today from "./components/today";
+
+
+export default function App() {
+  const ApiKey = "b6afdb8bf3154dbccb1600573f121ea4";
+  const [dailyData, setDailyData] = useState([]);
+  const [city, setCity] = useState("vijayawada");
+  const [search,setSearch] = useState(false)
+
+  useEffect(() => {
+    if (city){
+      if (search){
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${ApiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const dailyTemps = groupDataByDay(data.list);
+        setDailyData(dailyTemps);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+    }
+  });
+
+  const groupDataByDay = (data) => {
+    const dailyTemps = {};
+    const today = new Date().toLocaleDateString();
+
+    data.forEach((obj) => {
+      const date = convertToDate(obj.dt);
+      const condition = obj.weather[0].main;
+      const humidity = obj.main.humidity;
+      const wind = obj.wind.speed;
+      if (!dailyTemps[date]) {
+        dailyTemps[date] = {
+          maxTemp: obj.main.temp,
+          minTemp: obj.main.temp,
+          condition: condition,
+          humidity: humidity,
+          wind: wind,
+        };
+      } else {
+        dailyTemps[date].maxTemp = Math.max(
+          dailyTemps[date].maxTemp,
+          obj.main.temp
+        );
+        dailyTemps[date].minTemp = Math.min(
+          dailyTemps[date].minTemp,
+          obj.main.temp
+        );
+      }
+    });
+
+    const todayWeather = dailyTemps[today];
+    delete dailyTemps[today];
+    const nextFiveDaysWeather = Object.entries(dailyTemps).slice(0, 5);
+
+    return { today: todayWeather, nextFiveDays: nextFiveDaysWeather };
+  };
+
+  const convertToDate = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleDateString();
+  };
+
+  return (
+    <div className="App">
+      {/* search */}
+      <section>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              Weather App
+            </a>
+            <div className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Enter City"
+                aria-label="Search"
+                onChange={(e) => {
+                  setCity(e.target.value);
+                }}
+              />
+                <button class="btn btn-outline-success" type="submit" onClick={ () =>
+                  setSearch(true)
+                }>Search</button>
+            </div>
+          </div>
+        </nav>
+      </section>
+
+      <div className="container">
+          {dailyData.today && (
+            <Today
+              key="today"
+              date={city}
+              temps={dailyData.today}
+              condition={dailyData.today.condition}
+              humidity={dailyData.today.humidity}
+              wind={dailyData.today.wind}
+            />
+          )}
+        <div className="row">
+          {dailyData.nextFiveDays &&
+            dailyData.nextFiveDays.map(([date, temps]) => (
+              <Temp
+                key={date}
+                date={date}
+                temps={temps}
+                condition={temps.condition}
+                humidity={temps.humidity}
+                wind={temps.wind}
+              />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
